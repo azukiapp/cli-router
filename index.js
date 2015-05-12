ab78e85bc0a04e70d182008748d28182ee6f9fa7
Gullit Miranda <gullitmiranda@gmail.com>
2015-05-12 18:23:31 -0300

    [Kernel] Adding `index.js` to export classes

---
+++ index.js
@@ -0,0 +1,3 @@
+exports.Cli           = require('./lib/src').Cli;
+exports.CliRouter     = require('./lib/src/cli_router.js').CliRouter;
+exports.CliController = require('./lib/src/cli_controller.js').CliController;
